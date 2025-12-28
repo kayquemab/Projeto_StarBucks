159e0e5b271549c31c9af641acb78d0eb2bb2e55
Kayque Miqueias <kayquemiqueias17@gmail.com>
2025-12-28 16:35:37 -0300

    aadvadvvad

--- StarBucks/src/js/script.js
+++ StarBucks/src/js/script.js
@@ -7,3 +7,13 @@ mobileBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-xmark');
     icon.classList.toggle('fa-bars');
 });
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 0) {
+        header.style.boxShadow = '0 4px 10px rgba(0,0,0,0.1)';
+    } else {
+        header.style.boxShadow = 'none';
+    }
+})
